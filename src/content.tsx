2a8657c93ebdd5c36657a8b0e059b765e260d5a6
Keith Alpichi <keithalpichi@gmail.com>
2020-10-22 00:04:29 -0700

    Add search modal

--- src/content.tsx
+++ src/content.tsx
@@ -64,7 +64,26 @@ function LocationCard (props: React.PropsWithChildren<LocationCardProps>) {
   )
 }
 
+type ModalProps = {
+  visible: boolean
+  close: () => void
+}
+function Modal (props: React.PropsWithChildren<ModalProps>) {
+  if (!props.visible) {
+    return null
+  }
+  return (
+    <div className='h-screen w-screen top-0 left-0 fixed z-50' style={{ background: 'rgba(0,0,0,.75)' }} id='modal-parent' onClick={props.close}>
+      <div className='bg-white rounded-lg grid flex-column my-40 mx-auto max-w-2xl p-3 relative' id='modal-child' onClick={e => e.stopPropagation()}>
+        {props.children}
+      </div>
+    </div>
+  )
+}
+
 function App () {
+  const [_, updateSearchTerm] = React.useState<string>('')
+  const [modalVisible, updateModalVisibility] = React.useState<boolean>(false)
   const [error, setError] = React.useState<AppError | null>(null)
   const [location, setLocation] = React.useState<Location | null>(null)
   navigator.geolocation.getCurrentPosition(
@@ -85,15 +104,36 @@ function App () {
       }
     }
   )
+  const handleSearchUpdate = (e: React.ChangeEvent<HTMLInputElement>) => {
+    updateSearchTerm(e.target.value)
+  }
   return (
     <div className='app min-h-screen min-w-screen'>
+      <Modal visible={modalVisible} close={() => updateModalVisibility(false)}>
+        <div className='grid flex-column'>
+          <h1 className='font-sans text-2xl text-center'>Find a Spot</h1>
+          <form className='w-full rounded-md border'>
+            <input
+              className='w-full rounded-md p-3'
+              placeholder='City or beach'
+              maxLength={25}
+              type='search'
+              onChange={handleSearchUpdate}
+            />
+          </form>
+          <button onClick={() => updateModalVisibility(false)}>X</button>
+        </div>
+      </Modal>
       <h1 className='sm:col-span-12 md:col-span-2 font-sans font-bold text-4xl text-white'>Surf Report</h1>
       <p className='sm:col-span-12 md:col-span-8 text-center font-sans text-2xl text-white'>{dayjs().format('dddd, MMMM D, YYYY h:mm A')}</p>
       <button
         className='sm:col-span-12 md:col-span-2 transition-colors duration-100 ease-in-out text-gray-600 py-2 pr-4 pl-4 block w-full appearance-none leading-normal border border-transparent rounded-lg focus:outline-none text-left select-none truncate focus:bg-white focus:border-gray-300 bg-gray-200'
-        onClick={console.log}
+        onClick={() => updateModalVisibility(true)}
       >
-        Find a Spot
+        <svg width='20' height='20' className='inline mr-2' viewBox='0 0 20 20'><path d='M14.386 14.386l4.0877 4.0877-4.0877-4.0877c-2.9418 2.9419-7.7115 2.9419-10.6533 0-2.9419-2.9418-2.9419-7.7115 0-10.6533 2.9418-2.9419 7.7115-2.9419 10.6533 0 2.9419 2.9418 2.9419 7.7115 0 10.6533z' stroke='currentColor' fill='none' fillRule='evenodd' strokeLinecap='round' strokeLinejoin='round'></path></svg>
+        <span>
+          Find a Spot
+        </span>
       </button>
       <LocationCards className='col-span-2 row-start-2 row-end-5'>
         <LocationCard name='San Diego' active></LocationCard>
