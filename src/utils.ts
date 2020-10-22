8124f7abf5b38b88c5bde17cc59f26f1d8a76bef
Keith Alpichi <keithalpichi@gmail.com>
2020-10-22 00:03:46 -0700

    Add debounce & throttle

--- src/utils.ts
+++ src/utils.ts
@@ -17,3 +17,46 @@ export function classNames (...args: Array<string | classNameObject | undefined 
   }
   return result.join(' ')
 }
+
+export function debounce<F extends Function> (func: F, wait: number): F {
+  let timeoutID: number
+
+  if (!Number.isInteger(wait)) {
+    console.warn('Called debounce without a valid number')
+    wait = 300
+  }
+
+  // conversion through any necessary as it wont satisfy criteria otherwise
+  return function (this: any, ...args: any[]) {
+    clearTimeout(timeoutID)
+    const context = this
+
+    timeoutID = window.setTimeout(function () {
+		    func.apply(context, args)
+    }, wait)
+  } as any
+}
+
+export function throttle<F extends (...args: any[]) => any> (func: F, waitFor: number) {
+  const now = () => new Date().getTime()
+  const resetStartTime = () => startTime = now()
+  let timeout: NodeJS.Timeout
+  let startTime: number = now() - waitFor
+
+  return (...args: Parameters<F>): Promise<ReturnType<F>> =>
+    new Promise((resolve) => {
+      const timeLeft = (startTime + waitFor) - now()
+      if (timeout) {
+        clearTimeout(timeout)
+      }
+      if (startTime + waitFor <= now()) {
+        resetStartTime()
+        resolve(func(...args))
+      } else {
+        timeout = setTimeout(() => {
+          resetStartTime()
+          resolve(func(...args))
+        }, timeLeft)
+      }
+    })
+}
